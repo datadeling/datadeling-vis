969124e2bb2a71996d960891c89755f188ddde7c
Marius Watz <marius.watz@frog.co>
2023-02-01 03:43:05 +0100

    Delete Vite setup

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svgr(), react()],
-});
